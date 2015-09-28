689216000127894f6d4110e8a35a83810ebe2b30
Alex Hung <alexhung@ntnu.edu.tw>
2015-09-28 15:04:12 +0800

    清除舊的data/*/split*目錄

--- s5/combineHTK.sh
+++ s5/combineHTK.sh
@@ -10,10 +10,12 @@ copy-feats --htk-in=true scp:data/Aurora2.TR.Multi90/tmp.scp ark,scp:data/Aurora
 copy-feats --htk-in=true scp:data/Aurora2.TR.MultiDev10/tmp.scp ark,scp:data/Aurora2.TR.MultiDev10/feats.ark,data/Aurora2.TR.MultiDev10/feats.scp
 for train in $(for i in Clean90 CleanDev10 Multi90 MultiDev10; do echo Aurora2.TR.$i; done); do
 	echo $train
+	rm -rf data/${train}/split*
 	./utils/fix_data_dir.sh data/$train
 done
 for test in $(for i in A1 A2 A3 A4 B1 B2 B3 B4 C1 C2; do for j in C +20 +15 +10 +5 +0 -5; do echo Aurora2.TS.$i$j; done; done); do
 	echo $test
+	rm -rf data/${test}/split*
 	copy-feats --htk-in=true scp:data/${test}/tmp.scp ark,scp:data/${test}/feats.ark,data/${test}/feats.scp
 	./utils/fix_data_dir.sh data/${test}
 done
